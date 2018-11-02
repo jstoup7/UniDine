d3e6c9f5b08da70f8bcc108e9bf777e325af5957
jstoup7 <jstoup@udel.edu>
2018-11-02 19:43:38 -0400

    Added firebase core+auth and created basic login page
    Updated versions for firebase.  Made a mockup login page to test firebase authentication.  Able to login to account that is already added in firebase authentication page.  After logging in, takes you to a "welcome" screen. To test: email: test@gmail.com password: password

--- UniDine Code/app/src/main/java/com/example/unidine/MainActivity.java
+++ UniDine Code/app/src/main/java/com/example/unidine/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.unidine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void login(android.view.View view) {
+        EditText email = (EditText)findViewById(R.id.userEmail);
+        EditText password = (EditText)findViewById(R.id.userPassword);
+        mAuth.signOut();
+        mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    func();
+                }
+            }
+        });
+    }
+
+    public void func() {
+        TextView text = new TextView(this);
+        text.setText("Welcome " + mAuth.getCurrentUser().getEmail());
+        setContentView(text);
+    }
 }
+

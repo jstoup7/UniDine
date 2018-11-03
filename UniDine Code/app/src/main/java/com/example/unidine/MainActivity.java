77807dd35633c178aa11a39abdf1c6190534ee23
jstoup7 <jstoup@udel.edu>
2018-11-03 00:34:05 -0400

    Login screen now displays user's name.  Basic database for user info set up.
    When creating account you know can add in your name which is displayed on the welcome screen after logging in.  The basic database to store user info is now set up.

--- UniDine Code/app/src/main/java/com/example/unidine/MainActivity.java
+++ UniDine Code/app/src/main/java/com/example/unidine/MainActivity.java
@@ -3,18 +3,22 @@ package com.example.unidine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.AuthResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.tasks.OnFailureListener;
-
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,10 +55,15 @@ public class MainActivity extends AppCompatActivity {
     public void createAccount(android.view.View view) {
         EditText email = (EditText)findViewById(R.id.userEmail);
         EditText password = (EditText)findViewById(R.id.userPassword);
+        final EditText name = (EditText)findViewById(R.id.userName);
         mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(Task<AuthResult> task) {
                 if (task.isSuccessful()) {
+                    User user = new User(mAuth.getCurrentUser().getEmail());
+                    user.setName(name.getText().toString());
+                    DatabaseReference myRef = database.getReference("Users/" + mAuth.getCurrentUser().getUid());
+                    myRef.setValue(user);
                     func();
                 }
             }
@@ -74,10 +83,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    ValueEventListener postListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Get Post object and use the values to update the UI
+            setContentView(R.layout.activity_welcome);
+            TextView welcomeText = (TextView)findViewById(R.id.welcomeText);
+            welcomeText.setText("Welcome " + dataSnapshot.getValue());
+            // ...
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+    };
+
     public void func() {
-        setContentView(R.layout.activity_welcome);
-        TextView welcomeText = (TextView)findViewById(R.id.welcomeText);
-        welcomeText.setText("Welcome " + mAuth.getCurrentUser().getEmail());
+        DatabaseReference myRef = database.getReference("Users/" + mAuth.getCurrentUser().getUid() + "/name");
+        myRef.addListenerForSingleValueEvent(postListener);
     }
 }
 
---
+++ UniDine Code/app/src/main/java/com/example/unidine/User.java
@@ -0,0 +1,48 @@
+package com.example.unidine;
+
+public class User {
+    String name;
+    String email;
+    int age;
+    int radius;
+
+
+
+
+
+    public User(String e) {
+        this.email = e;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
